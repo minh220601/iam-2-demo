57bb16d5129e25baf4577778bf4ba8272e05bec0
minh2262001 <tranminh2262001@gmail.com>
2025-10-31 15:06:00 +0700

    add auditor configuration

--- src/main/java/com/demo/iam_demo/config/AuditConfig.java
+++ src/main/java/com/demo/iam_demo/config/AuditConfig.java
@@ -1,4 +1,9 @@
 package com.demo.iam_demo.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class AuditConfig {
 }
