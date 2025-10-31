aa238d40306b568acc3b091fb4efdf45e2325dff
minh2262001 <tranminh2262001@gmail.com>
2025-10-31 14:30:02 +0700

    add BaseEntity

--- src/main/java/com/demo/iam_demo/model/BaseEntity.java
+++ src/main/java/com/demo/iam_demo/model/BaseEntity.java
@@ -1,4 +1,43 @@
 package com.demo.iam_demo.model;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // xóa mềm
+    @Column(name = "is_deleted", columnDefinition = "boolean default false")
+    private boolean deleted = false;
+
+    // auditor
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime lastModifiedDate;
 }
