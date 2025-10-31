6893b3da2f03894ed7dff0aa86906c27f56e4e0d
minh2262001 <tranminh2262001@gmail.com>
2025-10-31 14:34:12 +0700

    add entity Role

--- src/main/java/com/demo/iam_demo/model/Role.java
+++ src/main/java/com/demo/iam_demo/model/Role.java
@@ -1,22 +1,38 @@
 package com.demo.iam_demo.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "roles")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+@EqualsAndHashCode(callSuper = true, exclude = {"permissions"})
+@ToString(callSuper = true, exclude = {"permissions"})
+@SQLDelete(sql = "UPDATE roles SET is_deleted = true, updated_at = NOW() WHERE id = ?")
+@Where(clause = "is_deleted = false")
+public class Role extends BaseEntity{
 
-    @Column(nullable = false,unique = true, length = 20)
+    @Column(nullable = false, unique = true, length = 50)
     private String name;
+
+    private String description;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "role_permissions",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+
+    )
+    @Builder.Default
+    private Set<Permission> permissions = new HashSet<>();
 }
