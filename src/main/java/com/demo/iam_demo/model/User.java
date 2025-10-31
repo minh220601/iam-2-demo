4adafc491cef86572429e61322c65d8d9a8a279b
minh2262001 <tranminh2262001@gmail.com>
2025-10-31 14:33:13 +0700

    add entity User

--- src/main/java/com/demo/iam_demo/model/User.java
+++ src/main/java/com/demo/iam_demo/model/User.java
@@ -2,10 +2,9 @@ package com.demo.iam_demo.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import java.time.LocalDate;
 import java.util.HashSet;
@@ -13,14 +12,14 @@ import java.util.Set;
 
 @Entity
 @Table(name = "users")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@SQLDelete(sql = "UPDATE users SET is_deleted = true, updated_at = NOW() WHERE id = ?") // logic soft delete
+@Where(clause = "is_deleted = false")
+public class User extends BaseEntity{
 
     @Column(nullable = false, unique = true, length = 254)
     @Email
@@ -31,14 +30,19 @@ public class User {
 
     @Column(nullable = false, unique = true, length = 50)
     private String username;
+
+    private String firstName;
+    private String lastName;
     private LocalDate birthDate;
     private String address;
     private String phone;
     private String avatar;
 
-    @Column(nullable = false)
-    private boolean active = true;
+    // khóa/ mở khóa user
+    @Column(name = "is_locked", columnDefinition = "boolean default false")
+    private boolean locked = false;
 
+    // gán vai trò RBAC
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
